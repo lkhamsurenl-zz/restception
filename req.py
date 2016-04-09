eef3661213857e46bc56ded21775f59333b3bede
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2016-04-08 22:07:26 -0500

    remove unnecessary files from the project

--- req.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/upload'
-files = {'file': open('/Users/lkhamsurenl/Downloads/icon.png', 'rb')}
-
-r = requests.post(url, files=files)
-
-print(r.text)
